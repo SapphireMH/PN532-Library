5c4299efa86efc3528eb0fbe833b857e950c8bb4
Mike Hoogendoorn <thisismike97@me.com>
2019-07-02 17:33:08 +0200

    polished documentation and added more functions

---
+++ Doxygen documents/pn532-doxygen-frontpage.hpp
@@ -0,0 +1,32 @@
+// ==========================================================================
+//
+// File      : pn532-doxygen-frontpage.hpp
+// Part of   : C++ library for controlling a PN532 chip over I2C or SPI.
+// Copyright : mike.hoogendoorn@student.hu.nl 2019
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// ==========================================================================
+
+// this file contains Doxygen lines.
+/// @file
+
+
+/// \brief
+/// PN532 library.
+
+/// \mainpage
+///
+/// \image html pn532-board.jpg
+///
+/// \author Mike Hoogendoorn (mike.hoogendoorn@student.hu.nl)
+/// \version 0.2 (last modified: 02-07-2019)
+/// \copyright boost license v1.0.
+///
+/// -------------------------------------------------------------------------
+/// 
+/// PN532 is a C++ object oriented library for close-to-the-hardware programming.
+/// This library is meant to be used with hwlib, which also requires bmptk.
+/// This library is written in C++ 2014.
--- main.cpp
+++ main.cpp
@@ -2,15 +2,58 @@
 
 int main() {
 
+	// Required pins to use this library with I2C.
 	auto scl = hwlib::target::pin_oc( hwlib::target::pins::scl );
 	auto sda = hwlib::target::pin_oc( hwlib::target::pins::sda );
-	auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda( scl, sda );
 	auto rst = hwlib::target::pin_out( hwlib::target::pins::d3 );
+	// Optional pin to save some bandwidth on the bus.
 	auto irq = hwlib::target::pin_in( hwlib::target::pins::d2 );
 	
-	pn532 object = pn532( i2c_bus, rst, irq );
+	// Create the object, "object" can be replaced with any name of your choosing.
+	pn532 object = pn532( scl, sda, rst, irq );
+	
+	// Fill this array with the card uid's you would like to use in your code.
+	// If your uid is only 4 bytes, pad it up with zeros as shown in this example.
+	std::array<std::array<uint8_t, 7>, 2> known_uid = {{{0xA4, 0x93, 0x4F, 0x12, 0x00, 0x00, 0x00},
+														{0x02, 0x21, 0x0B, 0x21, 0x00, 0x00, 0x00}}};
 
-	//object.get_firmware_version();
-	object.write_gpio( 0x00, 0x86 );
-
+	// Read the boards hardware and firmware version.
+	// Should always be: 
+	object.get_firmware_version();
+	
+	// Read the states of the gpio ports and places them into our array.
+	std::array<uint8_t, 3> gpio_states;
+	object.read_gpio( gpio_states );
+	
+	// Waits untill a card gets in range, reads its uid and places that into our array.
+	std::array<uint8_t, 7> uid;
+	object.get_card_uid( uid );
+		
+	// A loop to check if the received uid is known to us and does stuff
+	// based on whether or not we've added the uid into our array.
+	bool card_auth = false;
+	for(size_t i = 0; i < known_uid.size(); i++) {
+		
+		// The below if statement cal also be used with a specific uid
+		// replace the "i" with the array index of your choosing.
+		// Remember it starts at 0!
+		if( uid == known_uid[i] ) {
+			
+			// We do stuff if we have a known uid.
+			hwlib::cout << "Found trusted uid!\n";
+			// Set GPIO P72 to HIGH, in my case we light up a blue LED.
+			object.write_gpio( 0x00, 0x84 );
+			// Set a bool so the code below wont run if we detected a known uid.
+			card_auth = true;
+			
+		}
+	}
+	// If no known card is found the following code gets run.
+	if( !card_auth ) {
+		
+		hwlib::cout << "No known card detected!\n";
+		// Set GPIO P71 to HIGH, in my case we light up a red LED.
+		object.write_gpio( 0x00, 0x82 );
+		
+	}
 }
